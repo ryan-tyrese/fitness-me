fbd6ca60b18ac2085d83e3ff122646d97a443eab
ryan-tyrese <ryantyresen@gmail.com>
2025-05-22 16:37:22 +0200

    Navbar functionality and Hero section
    Need to fix dropdown Menu(cooked by position(absolute and relative)

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,19 @@
 import './App.css'
+import sImage from '../src/assets/woman-jump.jpg'
+import bImage from '../src/assets/man-rope.jpg'
 
 function App() {
   return (
     <>
-      <div className='w-full h-full absolute bg-gradient-to-r from-gray-500 to-white'></div>
+      <div className='relative pb-2 h-96'>
+        <img src={sImage} alt="image" className='lg:hidden block w-full h-full shadow-lg shadow-black object-cover object-top'/>
+        <img src={bImage} alt="image" className='hidden lg:block w-full h-full object-cover shadow-lg shadow-black'/>
+        <div className='absolute bottom-5 left-5'>
+          <h3 className='text-3xl sm:text-4xl lg:text-5xl xl:text-6xl font-bold font-sans text-black lg:text-white'>Get Sweaty, Get...</h3>
+          <h3 className='text-3xl sm:text-4xl lg:text-5xl xl:text-6xl font-bold font-sans text-black lg:text-white'>HOT</h3>
+          <p className='text-black lg:text-white font-semibold'>Get fit for <b>THAT</b> summer pic.</p>
+        </div>
+      </div>
     </>
   )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,10 +6,10 @@ export default function Navbar(){
 
     return(
         <>
-            <header className="flex items-center justify-between py-4 px-6 lg:px-14 text-black drop-shadow-md">
+            <header className="bg-white flex items-center justify-between py-4 px-6 lg:px-14 text-black drop-shadow-md">
                 {/*Logo*/}
                 <a href="#">
-                    <span className="text-2xl font-bold lg:text-3xl">Fitness Me</span>
+                    <span className="text-2xl font-bold lg:text-3xl">Fit</span>
                 </a>
                 {/*Links*/}
                 <ul className="hidden lg:flex justify-between gap-4 font-semibold text-base">
@@ -18,13 +18,13 @@ export default function Navbar(){
                     <li className="p-2 hover:text-gray-500 transition-all cursor-pointer">About</li>
                 </ul>
                 {/*Menu*/}
-                <i className="lg:hidden block">
-                    <img src={hamburger} alt="Menu" className="w-8 h-8 text-5xl cursor-pointer" onClick={() => setIsMenuOpen(!isMenuOpen)}/>
-                </i>
-                <div className={`absolute xl:hidden top-24 left-0 w-full bg-white flex flex-col items-center gap-6 font-semibold text-lg transform transition-transform ${isMenuOpen ? "opacity-100" : "opacity-0"}`} style={{transition: "transform 0.3s ease, opacity 0.3s ease"}}>
-                    <li className="list-none w-full text-center p-4 hover:text-gray-500 transition-all cursor pointer">Exercises</li>
-                    <li className="list-none w-full text-center p-4 hover:text-gray-500 transition-all cursor pointer">Create</li>
-                    <li className="list-none w-full text-center p-4 hover:text-gray-500 transition-all cursor pointer">About</li>
+                <button className="lg:hidden block" onClick={() => setIsMenuOpen(!isMenuOpen)}>
+                    <img src={hamburger} alt="Menu" className="w-8 h-8 text-5xl cursor-pointer"/>
+                </button>
+                <div className={`absolute xl:hidden top-15 left-0 w-full bg-white flex flex-col items-center gap-2 font-semibold text-lg transform transition-transform ${isMenuOpen ? "opacity-100" : "opacity-0"}`} style={{transition: "transform 0.3s ease, opacity 0.3s ease"}}>
+                    <li className="list-none w-full text-center p-4 hover:text-gray-500 transition-all cursor-pointer">Exercises</li>
+                    <li className="list-none w-full text-center p-4 hover:text-gray-500 transition-all cursor-pointer">Create</li>
+                    <li className="list-none w-full text-center p-4 hover:text-gray-500 transition-all cursor-pointer">About</li>
                 </div>
             </header>  
         </>
